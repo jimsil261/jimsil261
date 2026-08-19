f1ee0b49bd5f7d4ac73a2ce072407d57be9d586b
Jae Ryeol Kim <144120997+jimsil261@users.noreply.github.com>
2026-08-19 20:51:41 +0900

    [Passed] 사각형의 총 넓이 2 (67ms, 17MB)

---
+++ trail2/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -0,0 +1,21 @@
+n = int(input())
+x1, y1, x2, y2 = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    x1.append(a)
+    y1.append(b)
+    x2.append(c)
+    y2.append(d)
+
+# Please write your code here.
+square=[[0]*201 for _ in range(201)]
+for i in range(n):
+    for x in range(x1[i],x2[i],1):
+        for y in range(y1[i],y2[i],1):
+            square[x+100][y+100]=1
+count=0
+for i in range(201):
+    for j in range(201):
+        if square[i][j]==1:
+            count+=1
+print(count)
