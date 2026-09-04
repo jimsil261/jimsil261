3535eb69aa1caaa889ed7ae3068c7c55c4bcc0ed
Jae Ryeol Kim <144120997+jimsil261@users.noreply.github.com>
2026-09-04 20:37:41 +0900

    [Passed] 키를 기준으로 정렬 (51ms, 16MB)

---
+++ trail2/키를 기준으로 정렬/sort-by-height.py
@@ -0,0 +1,27 @@
+n = int(input())
+name = []
+height = []
+weight = []
+
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+
+class Person:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+    
+people = []
+
+for i in range(n):
+    people.append(Person(name[i], height[i], weight[i]))
+
+people.sort(key=lambda x: x.height)
+
+for person in people:
+    print("%s %d %d" %(person.name,person.height,person.weight))
