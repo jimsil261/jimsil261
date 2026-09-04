a26e7bc8663689925603ac6dea5b3ea529f49e20
Jae Ryeol Kim <144120997+jimsil261@users.noreply.github.com>
2026-09-04 20:53:44 +0900

    [Passed] 줄 세우기 (50ms, 16MB)

---
+++ trail2/줄 세우기/line-up-students.py
@@ -0,0 +1,11 @@
+n = int(input())
+students = [tuple(map(int, input().split())) + (i + 1,) for i in range(n)]
+
+# Please write your code here
+# Please write your code here.
+
+students.sort(key=lambda x: (-x[0],-x[1],x[2] ))
+
+for student in students:
+    print(student[0], student[1], student[2])
+
